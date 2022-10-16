cff47fc45566c90edd58c55a3ad497af2b641f95
animeshvashisth24 <84134397+animeshvashisth24@users.noreply.github.com>
2022-10-16 17:20:50 +0530

    Add files via upload

---
+++ Recursion/factorial.cpp
@@ -0,0 +1,21 @@
+// factorial using recursion
+#include <bits/stdc++.h>
+using namespace std;
+
+int fac(int n)
+{
+  if (n == 0)
+  {
+    return 1;
+  }
+  else
+  {
+    return fac(n - 1) * n;
+  }
+}
+int main()
+{
+  int a = fac(3);
+  cout << a << "\n";
+  return 0;
+}
