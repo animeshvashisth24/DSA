592f4078fb8ea2438b87b818d2856026107ad6d8
animeshvashisth24 <84134397+animeshvashisth24@users.noreply.github.com>
2022-10-19 22:50:47 +0530

    Add files via upload

---
+++ Recursion/towerOfHanoi.cpp
@@ -0,0 +1,19 @@
+// tower of hanoi using recursion - O(2^n)
+#include <bits/stdc++.h>
+using namespace std;
+
+int TOH(int n, int A, int B, int C)
+{
+  if (n > 0)
+  {
+    TOH(n - 1, A, C, B);
+    cout << A << "->" << C << "\t";
+    TOH(n - 1, B, A, C);
+  }
+  return 0;
+}
+int main()
+{
+  TOH(3, 1, 2, 3);
+  return 0;
+}
