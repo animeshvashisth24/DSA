ba70d52ef396a671954fcc111285b35161276a0b
animeshvashisth24 <84134397+animeshvashisth24@users.noreply.github.com>
2022-10-21 19:46:58 +0530

    Update taylorSeries.cpp

--- Recursion/taylorSeries.cpp
+++ Recursion/taylorSeries.cpp
@@ -30,7 +30,7 @@ double E(int x, int n)
   }
   else
   {
-    s = 1 + s * x / n; // (x/n) needs typecasting as both will give int division so first multiply num with s to get write ans.
+    s = 1 + s * x / n;// (x/n) needs typecasting as both will give int division so first multiply num with s to get write ans.
     return E(x, n - 1);
   }
 }
