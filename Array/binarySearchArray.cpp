4212ea48509a20a4d0fd9fef7f441e092ce14b09
animeshvashisth24 <84134397+animeshvashisth24@users.noreply.github.com>
2022-11-02 23:03:08 +0530

    Add files via upload

---
+++ Array/binarySearchArray.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+int size = 10;
+int length = 10;
+
+int binarySearch(int arr[], int low, int high, int key)
+{
+
+  while (low <= high)
+  {
+    int mid = (low + high) / 2;
+    if (key == arr[mid])
+    {
+      return mid;
+    }
+    else if (key < arr[mid]) // commented approac - recursion binary search
+    {
+      high = mid - 1; // return binarySearch(arr, low, mid - 1, key);
+    }
+    else
+    {
+      low = mid + 1; // return binarySearch(arr, mid + 1, high, key);
+    }
+  }
+  return -1; // not found
+}
+int main()
+{
+  int key;
+  int arr[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+  // cout << "\n enter key element to search : ";
+  // cin >> key;
+  int low = 0;
+  int high = length - 1;
+  int result = binarySearch(arr, low, high, 50);
+  if (result != -1)
+    cout << "\nelement found at index " << result << endl;
+  else
+    cout << "\nelement not found" << endl;
+  return 0;
+}
