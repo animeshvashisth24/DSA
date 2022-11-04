877c623bce9d2cd8cf95932d2df8ed9446357093
animeshvashisth24 <84134397+animeshvashisth24@users.noreply.github.com>
2022-11-04 23:35:28 +0530

    Add files via upload

---
+++ Array/leftShiftArray.cpp
@@ -0,0 +1,31 @@
+// left shift array by one(loosing first element value)-- looks like scrolling
+#include <bits/stdc++.h>
+using namespace std;
+int size = 10;
+int length = 5;
+void display(int arr[])
+{
+  for (int i = 0; i < length; i++)
+  {
+    cout << "arr[" << i << "] = " << arr[i] << "  ";
+  }
+  cout << endl;
+}
+int leftShiftByOne(int arr[])
+{
+  for (int i = 0; i < length; i++)
+  {
+    arr[i] = arr[i + 1];
+  }
+  return 0;
+}
+int main()
+{
+  int arr[10] = {2, 4, 6, 8, 10};
+  cout << "\n array (before) : " << endl;
+  display(arr);
+  leftShiftByOne(arr);
+  cout << "\n array after one left shift : " << endl;
+  display(arr);
+  return 0;
+}
